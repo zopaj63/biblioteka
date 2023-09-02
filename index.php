4c0f886249b43cda79d9c17380c75c8b52e4853e
zopaj63 <zoran.pajnic@gmail.com>
2023-09-02 15:52:10 +0200

    debuging, krivi naziv kolone

--- index.php
+++ index.php
@@ -6,16 +6,18 @@
     <title>Biblioteka</title>
 </head>
 <body>
+    <a href="https://github.com/zopaj63/biblioteka">Git</a>
+    
     <h1>Biblioteka</h1>
 
     <h3>Forma za unos knjige:</h3>
     <fieldset style="width:40%; padding: 10px;">
         <form action="" method="POST">
             <label>Naslov knjige</label><br>
-            <input type="text" name="naslov" id="naslov" require/>
+            <input type="text" name="naslov" id="naslov" required/>
             <br><br>
             <label>Odaberite autora:</label><br>
-            <select name="id_autori" id="id_autori" require>
+            <select name="id_autori" id="id_autori" required>
                 <option value="1">Robin Nixon</option>
                 <option value="2">Matt Stauffer</option>
                 <option value="3">Matt Zandstra</option>
@@ -26,7 +28,7 @@
             </select>
             <br><br>
             <label>Godina izdanja</label><br>
-            <input type="number" name="godina" id="godina" value="2023" min="1750" max="2050" require/>
+            <input type="number" name="godina" id="godina" value="2023" min="1750" max="2050" required/>
             <br><br>
             <input type="submit" name="dodaj" value="Dodaj knjigu"/>
         </form>
@@ -35,10 +37,14 @@
 
 
     <?php
+
+    //debuging
+    //error_reporting(E_ALL);
+    //ini_set('display_errors', 1);
     
     require "autoload.php";
 
-    $config=new Config("config.ini");
+    $config=new Config("local_config.ini");
     $db=Database::getInstance($config);
     $conn=$db->getConnection();
 
@@ -58,19 +64,27 @@
         $naslov = $_POST['naslov'];
         $id_autori = $_POST['id_autori'];
         $godina_izdanja = $_POST['godina'];
-    
-        $stmt=$conn->prepare("INSERT INTO knjige (naslov, id_autori, godina) VALUES (:naslov, :id_autori, :godina)");
-        $stmt->bindParam(":naslov", $naslov);
-        $stmt->bindParam(":id_autori", $id_autori);
-        $stmt->bindParam(":godina", $godina);
 
-        if($stmt->execute())
+        if (empty($naslov) || empty($id_autori) || empty($godina_izdanja)) 
         {
-            echo "Knjiga je uspješno dodana";
-        }
-        else
+            echo "Molimo popunite sva polja.";
+        } 
+        else 
         {
-            echo "Došlo je do greške";
+            $stmt=$conn->prepare("INSERT INTO knjige (naslov, id_autori, godina_izdanja) VALUES (:naslov, :id_autori, :godina_izdanja)");
+            $stmt->bindParam(":naslov", $naslov);
+            $stmt->bindParam(":id_autori", $id_autori);
+            $stmt->bindParam(":godina_izdanja", $godina_izdanja);
+
+                if($stmt->execute())
+                {
+                    echo "Knjiga je uspješno dodana" ."<br>" ."<br>";
+                }
+                else
+                {
+                    echo "Došlo je do greške: " . $stmt->errorInfo()[2];
+                }
+
         }
     }
 
